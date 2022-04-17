8d6762dca18eeb520b92ab75687dbaa1c59cf080
EvgrafovMichal <autumnevilmen@gmail.com>
2022-04-17 17:10:44 +0300

    delete MatrixMultiplicator.h, add Matrix

--- MLP/constants.h
+++ MLP/constants.h
@@ -1,7 +1,4 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
-#include <vector>
-
-using matrix = std::vector<std::vector<double>>;
 
 #endif // CONSTANTS_H
--- MLP/layers/linear.h
+++ MLP/layers/linear.h
@@ -1,19 +1,21 @@
 #ifndef LINEAR_H
 #define LINEAR_H
-#include "../multiplicators/MatrixMultiplicator.h"
+#include "../matrix/matrix.h"
 #include "layer.h"
+
+using std::size_t;
 
 class Linear : public Layer
 {
   private:
-    matrix W;
-    matrix bias;
-    MatrixMultiplicator *mm;
+    Matrix W;
+    Matrix bias;
+    Matrix *mm;
 
   public:
-    Linear(size_t in, size_t out, bool bias, MatrixMultiplicator *_mm);
-    matrix forward(const matrix &X) override;
-    matrix backward(const matrix &grads) override;
+    Linear(size_t in, size_t out, bool bias, Matrix *_mm);
+    Matrix forward(const Matrix &X) override;
+    Matrix backward(const Matrix &grads) override;
 };
 
 #endif // LINEAR_H
