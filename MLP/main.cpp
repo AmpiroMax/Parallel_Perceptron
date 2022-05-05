e8e17e9dede081c16050a18d76cd62b85504c4d8
AmpiroMax <patrat2015@yandex.ru>
2022-05-05 23:50:01 +0300

    Matplotlib was added for the project. Perfectly works on my machine.

--- MLP/main.cpp
+++ MLP/main.cpp
@@ -3,10 +3,31 @@
 #include "functionality.h"
 #include "gstring/GString.h"
 #include "matrix/Matrix.h"
+#include "perceptron.h"
+
+#include "matplotlibcpp.h"
+namespace plt = matplotlibcpp;
+
+/*
+
+▒▒▒▒▒▒▒▒█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█▒▒▒▒▒▒
+▒▒▒▒▒▒▒█░▒▒▒▒▒▒▒▓▒▒▓▒▒▒▒▒▒▒░█▒▒▒▒▒
+▒▒▒▒▒▒▒█░▒▒▓▒▒▒▒▒▒▒▒▒▄▄▒▓▒▒░█░▄▄▒▒
+▒▒▄▀▀▄▄█░▒▒▒▒▒▒▓▒▒▒▒█░░▀▄▄▄▄▄▀░░█▒
+▒▒█░░░░█░▒▒▒▒▒▒▒▒▒▒▒█░░░░░░░░░░░█▒
+▒▒▒▀▀▄▄█░▒▒▒▒▓▒▒▒▓▒█░░░█▒░░░░█▒░░█
+▒▒▒▒▒▒▒█░▒▓▒▒▒▒▓▒▒▒█░░░░░░░▀░░░░░█
+▒▒▒▒▒▄▄█░▒▒▒▓▒▒▒▒▒▒▒█░░█▄▄█▄▄█░░█▒
+▒▒▒▒█░░░█▄▄▄▄▄▄▄▄▄▄█░█▄▄▄▄▄▄▄▄▄█▒▒
+▒▒▒▒█▄▄█░░█▄▄█░░░░░░█▄▄█░░█▄▄█▒▒▒▒
+
+*/
 
 int main()
 {
-    testDataloader();
-
+    // Perceptron model(784);
+    // trainLoop(model);
+    plt::plot({1, 3, 2, 4});
+    plt::show();
     return 0;
 }
